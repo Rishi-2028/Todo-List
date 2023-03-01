d2937a94db45816de4fe33fbd1ab73233bfcbe5e
Rishi-2028 <rishibhandari2028@gmail.com>
2023-03-01 19:51:59 +0545

    Creating Model

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Rishi on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
